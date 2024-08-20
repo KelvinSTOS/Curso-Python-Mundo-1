7a8ce57596eece0eb1b006308d0b203c19e721c7
Kelvin Santos de Abreu <kalelcripton62@gmail.com>
2024-08-20 19:35:11 -0300

    Create Exercício Python #022 - Analisador de Textos.py

---
+++ Exercício Python #022 - Analisador de Textos.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite o seu nome completo:')).strip()
+print('Analisando o seu nome...')
+print('O seu nome em letra maíuscula é: {}'.format(nome.upper()))
+print('O seu nome em letrar minusculas é:{}'.format(nome.lower()))
+print('Seu nome tem ao tanto {} letras'.format(len(nome)- nome.count(' ')))
+#print('O seu primeiro nome possui {} letras'.format(nome.find(' ')))
+separador = nome.split.nome()
+print('O seu nome é {} e ele possui {} letras'.format(separador[0], len(separador[0])))
