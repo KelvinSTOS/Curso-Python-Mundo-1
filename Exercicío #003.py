e819e9893fb5d99a7f46ce943412f6b11b5cdccf
Kelvin Santos de Abreu <kalelcripton62@gmail.com>
2024-08-20 19:24:31 -0300

    Create Exercicío #003.py
    Nesse exercício aprendi como definir controle de informações com variáveis

---
+++ Exercicío #003.py
@@ -0,0 +1,9 @@
+nome = 'kelvin'
+date = 25
+peso = 75
+print("O", nome, "Tem", date, "Anos de idade e", peso, "De peso")
+
+nome = input('Qual é o seu nome?')
+date = input('Qual sua idade?')
+peso = input('Qual é o seu peso?')
+print('O seu nome é', nome, 'sua idade e', date, 'e seu peso é ', peso,)
