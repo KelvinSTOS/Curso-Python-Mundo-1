3e1b2dacf9e11c2b7e482fe633ea7f0eb0e2b7be
Kelvin Santos de Abreu <kalelcripton62@gmail.com>
2024-08-20 19:32:07 -0300

    Exercício Python #011 - Pintando Parede.py

---
+++ Exercício Python #011 - Pintando Parede.py
@@ -0,0 +1,6 @@
+la = float(input('Qual a largura da aprede?'))
+al = float(input('Qual a altura da parede?'))
+ar = la * al
+print('A largura de {:.2f}M², e sua altura é de {:.2f}m², a  área de sua parece é de {}'.format(la, al, ar))
+tin = ar / 2
+print('E para essa áre você precisará de {}L de tinta.'.format(tin))
