44fd740503590afaac049dbfce6e5547bbefae13
Kelvin Santos de Abreu <kalelcripton62@gmail.com>
2024-08-20 19:26:45 -0300

    Desafio da Aula #003.py

---
+++ Desafio da Aula #003.py
@@ -0,0 +1,6 @@
+n1 = input('Digite o 1° valor')
+n2 = input('Digite o 2° valor')
+n3 = input('Digite o 3° valor')
+n4 = input('Digite o 5° valor')
+S = n1 +n2 + n3 + n4
+print(f'A soma dos valores {n1} + {n2} + {n3} + {n4}, vale {S}')
